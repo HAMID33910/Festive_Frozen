a19a2c3888208c07e2cd2db0e8532d8b91b3eef5
HAMID33910 <thehamidbadshadon@gmail.com>
2026-07-22 23:22:39 +0500

    update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_URL = import.meta.env.VITE_API_URL || (window.location.hostname === "localhost" ? 
     "http://localhost:3001" : 
-    "https://festive-frozen-api.northflank.app");
+    "https://festive-frozen.up.railway.app");
 
 export { API_URL };
